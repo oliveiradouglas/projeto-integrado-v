f9c82261c75378d7a0e0b3a93eb2140748f113fe
Douglas Oliveira <d.oliveira12@hotmail.com>
2016-11-12 22:08:10 -0200

    Tipo de usuário

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Cliente extends Model {
     protected $table = 'cliente';
 
+    protected $fillable = ['id_usuario'];
+
     public function getCartoes() {
     	return Cartao::where('id_cliente', $this->id)->get();
     }
--- app/Models/Motoboy.php
+++ app/Models/Motoboy.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Motoboy extends Model {
     protected $table = 'motoboy';
 
+    protected $fillable = ['id_usuario', 'status'];
+    
     public function getStatus() {
     	return new \App\Source\Motoboy\Status($this->status);
     }
